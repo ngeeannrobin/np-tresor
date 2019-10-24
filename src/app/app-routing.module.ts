e4f5973c4b2e46465fe421708e8727da92717fdf
ngeeannrobin <s10179209@connect.np.edu.sg>
2019-10-24 21:19:25 +0800

    removed AR route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ARComponent } from './ar/ar.component';
 import { ViewQuestComponent } from './view-quest/view-quest.component';
 
 const routes: Routes = [
-  {path: "ar", component: ARComponent},
   {path: "ViewQuest", component: ViewQuestComponent},
   {path: "", pathMatch: "full", redirectTo:"ViewQuest"}
 ];
