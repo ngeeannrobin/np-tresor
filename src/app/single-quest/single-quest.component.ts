067f96119ccbe656b9fa5ee080a6d51ea1a86f0f
ngeeannrobin <s10179209@connect.np.edu.sg>
2019-10-29 23:10:09 +0800

    added & implemented viewing of a single quest.

---
+++ src/app/single-quest/single-quest.component.ts
@@ -0,0 +1,86 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { RealtimedatabaseService } from '../realtimedatabase.service';
+import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
+import { Location } from '@angular/common';
+
+@Component({
+  selector: 'app-single-quest',
+  templateUrl: './single-quest.component.html',
+  styleUrls: ['./single-quest.component.css'],
+})
+export class SingleQuestComponent implements OnInit {
+
+  constructor(
+    private route: ActivatedRoute,
+    private db: RealtimedatabaseService,
+    private dialog: MatDialog,
+    private location: Location) { }
+  questId: string;
+  quest: any = {};
+  scanning: boolean = false;
+  ngOnInit() {
+    this.questId = this.route.snapshot.paramMap.get("id");
+    this.FetchQuest(this.questId);
+  }
+
+  FetchQuest(id){
+    this.db.FetchSingleQuest(id).then(
+      quest => {
+        this.quest = quest;
+      },
+      err => {
+        console.log(err);
+      }
+    )
+  }
+
+
+  // using two functions instead of toggling
+  // in case user taps an even number of times.
+  OpenCamera(){this.scanning = true;}
+  CloseCamera(){this.scanning = false;}
+
+  ScanCallback(qr_data){
+    this.CloseCamera();
+    if (this.CheckQR(qr_data)){
+      this.popCongratsMessageDialog();
+    }
+  }
+
+  CheckQR(qr_data):boolean{
+    return qr_data == this.questId;
+  }
+
+  popCongratsMessageDialog(){
+    const dialogRef = this.popMessageDialog(
+      "Congratulations! You did it! Yatta desu ne!",
+      "(dismiss this to return to quest list)"
+    );
+
+    dialogRef.afterClosed().subscribe(
+      res => {
+        this.location.back();
+      }
+    )
+  }
+
+  popMessageDialog(message:string, submessage:string){
+    const dialogRef = this.dialog.open(SingleQuestMessageDialog, {
+      width: "75%",
+      data: {msg: {main: message, sub: submessage}}
+    });
+    return dialogRef;
+  }
+
+}
+
+// Message dialog; no functionalities
+@Component({
+  selector: 'SingleQuestMessage',
+  templateUrl: 'single-quest-message.dialog.html',
+  styleUrls: ['single-quest.component.css']
+})
+export class SingleQuestMessageDialog {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}
+}
