6528cd4d6e0793646a1bb807f8151db5ed532c15
ngeeannrobin <s10179209@connect.np.edu.sg>
2019-10-30 10:52:14 +0800

    attempt to fix ios pwa camera bug

--- src/app/single-quest/single-quest.component.ts
+++ src/app/single-quest/single-quest.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { RealtimedatabaseService } from '../realtimedatabase.service';
 import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
 import { Location } from '@angular/common';
+import { ZXingScannerComponent } from '@zxing/ngx-scanner';
 
 @Component({
   selector: 'app-single-quest',
@@ -24,6 +25,12 @@ export class SingleQuestComponent implements OnInit {
   ngOnInit() {
     this.questId = this.route.snapshot.paramMap.get("id");
     this.FetchQuest(this.questId);
+    
+  }
+
+  CheckPermission(){
+    let scanner = new ZXingScannerComponent();
+    scanner.askForPermission()
   }
 
   FetchQuest(id){
@@ -40,7 +47,7 @@ export class SingleQuestComponent implements OnInit {
 
   // using two functions instead of toggling
   // in case user taps an even number of times.
-  OpenCamera(){this.scanning = true;}
+  OpenCamera(){this.CheckPermission();this.scanning = true;}
   CloseCamera(){this.scanning = false;}
 
   ToggleScanLine(){this.scanEffect = (this.scanEffect + 1) % 4;}
