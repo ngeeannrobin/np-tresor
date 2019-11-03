90abf15dbe62481da3e32a51d26ca771ffe00056
ngeeannrobin <s10179209@connect.np.edu.sg>
2019-11-03 23:49:53 +0800

    removed useless code & files

--- src/app/single-quest/single-quest.component.ts
+++ src/app/single-quest/single-quest.component.ts
@@ -1,10 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { RealtimedatabaseService } from '../realtimedatabase.service';
-import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
 import { Location } from '@angular/common';
-import { ZXingScannerComponent } from '@zxing/ngx-scanner';
-import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-single-quest',
@@ -16,7 +13,6 @@ export class SingleQuestComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private db: RealtimedatabaseService,
-    private dialog: MatDialog,
     private location: Location) { }
   questId: string;
   quest: any = {hint:{}};
@@ -64,53 +60,38 @@ export class SingleQuestComponent implements OnInit {
     )
   }
 
-async ToggleCamera(){
-  if (this.animate==0){ //no animation playing
-    if (this.showCamera){ // camera is open / hint is not showing
-      this.animate = -1;
-      this.showHint = true;
-      await this.delay(500);
-      this.showCamera = false;
-    } else { // camera not open / hint is showing
-      this.showCamera = true;
-      this.animate = 1;
-      await this.delay(1500);
-      this.showHint = false;
+  async ToggleCamera(){
+    if (this.animate==0){ //no animation playing
+      if (this.showCamera){ // camera is open / hint is not showing
+        this.animate = -1;
+        this.showHint = true;
+        await this.delay(500);
+        this.showCamera = false;
+      } else { // camera not open / hint is showing
+        this.showCamera = true;
+        this.animate = 1;
+        await this.delay(1500);
+        this.showHint = false;
+      }
+      this.animate = 0;
     }
-    this.animate = 0;
   }
-}
 
   ScanCallback(qr_data){
     console.log(qr_data);
     this.ToggleCamera();
     if (this.CheckQR(qr_data)){
-      // this.popCongratsMessageDialog();
       this.message = "You got it! Tap anywhere to return back to quests!"
       this.correct = true;
     } else {
       this.message = "That's ain't it, chief! Tap anywhere to continue."
       this.correct = false;
-      // this.popMessageDialog("You wrong, you lose, so best think you haven't won.","");
     }
   }
 
   CheckQR(qr_data):boolean{
     return qr_data == this.questId;
   }
-
-  // popCongratsMessageDialog(){
-  //   const dialogRef = this.popMessageDialog(
-  //     "Congratulations! You did it! Yatta desu ne!",
-  //     "(dismiss this to return to quest list)"
-  //   );
-
-  //   dialogRef.afterClosed().subscribe(
-  //     res => {
-  //       this.back();
-  //     }
-  //   )
-  // }
 
   back(){
     this.location.back();
@@ -129,31 +110,9 @@ async ToggleCamera(){
         this.showMessage = false;
         this.animate = 0;
       }
-      
     }
-
   }
-
-  popMessageDialog(message:string, submessage:string){
-    const dialogRef = this.dialog.open(SingleQuestMessageDialog, {
-      width: "75%",
-      data: {msg: {main: message, sub: submessage}}
-    });
-    return dialogRef;
-  }
-
   async delay(ms: number) {
     return new Promise( resolve => setTimeout(resolve, ms));
   }
-
-}
-
-// Message dialog; no functionalities
-@Component({
-  selector: 'SingleQuestMessage',
-  templateUrl: 'single-quest-message.dialog.html',
-  styleUrls: ['single-quest.component.css']
-})
-export class SingleQuestMessageDialog {
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}
 }
