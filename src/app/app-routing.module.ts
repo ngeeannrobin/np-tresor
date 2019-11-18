68e6c4aac39eabeafd539f5f625fdc2aa6c1e164
shleen <sim.sheline@gmail.com>
2019-11-19 02:19:46 +0800

    add route for profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,11 @@ import { LoginComponent } from './login/login.component';
 import { ViewQuestComponent } from './view-quest/view-quest.component';
 import { SingleQuestComponent } from './single-quest/single-quest.component';
 import { CuriousUserComponent } from './curious-user/curious-user.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
   {path: "login", component: LoginComponent},
+  {path: "profile", component: ProfileComponent},
   {path: "ViewQuest", component: ViewQuestComponent},
   {path: "ViewQuest/:id",component: SingleQuestComponent},
   {path: "qr/:random",component: CuriousUserComponent},
