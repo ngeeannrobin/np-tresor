4eb41b8b5e89cf2fd4cca6385c46d56c5ca324be
shleen <sim.sheline@gmail.com>
2019-11-19 18:25:42 +0800

    create profile

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as firebase from 'firebase/app';
+import { RealtimedatabaseService } from '../realtimedatabase.service';
+import { NumberFormatStyle, Location } from '@angular/common';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
@@ -7,9 +12,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  photoUrl:string;
+  name:string;
+  email:string;
+  id:string;
+
+  total_points:number;
+  quests_completed:NumberFormatStyle;
+
+  defaultPhotoUrl = "../../assets/image/default_avatar.png";
+
+  constructor(
+    public db: RealtimedatabaseService,
+    private router: Router,
+    private location: Location
+  ) { }
 
   ngOnInit() {
+    var user = firebase.auth().currentUser;
+
+    if (user != null) {
+      this.name = user.displayName;
+      this.email = user.email;
+      this.photoUrl = user.photoURL;
+      this.id = user.uid;
+    }
+
+    this.db.GetUserPoints(this.id).then(pts => {
+      this.total_points = pts;
+    });
+
+    this.db.GetUserCompleteQuests(this.id).then(quests => {
+      this.quests_completed = quests.length;
+    });
+    
+  }
+
+  goBack() {
+    this.location.back();
+  }
+
+  signOut() {
+    firebase.auth().signOut();
+    this.router.navigate(['/login']);
   }
 
 }
